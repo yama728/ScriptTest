f31b247fe08e2f56a3bdc2fbe0c6519a12d9a63c
natsuya yamashiro <azuki728@me.com>
2019-10-07 18:05:18 +0900

    Lesson4の課題です。要素の個数が5のint型の配列arrayを宣言し、for文で配列の各要素の値を順番に表示した後に配列の各要素の値を逆順に表示させています。これから発展課題に取り掛かります。

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 要素数5の配列を初期化する
+		int[] array = new int[5];
+
+		// 配列の各要素に値を代入する
+		array [0] = 30;
+		array [1] = 20;
+		array [2] = 50;
+		array [3] = 10;
+		array [4] = 80;
+
+		// 配列の要素をすべて表示する
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array [i]);
+		}
+
+		//配列の要素を逆順に表示する
+		for (int i = array.Length - 1; 0 <= i; i--) {
+			Debug.Log (array [i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
